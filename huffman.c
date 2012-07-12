672669734b3419636cc509af7fa52c99be97ef64
Hat <zhohuang@sigma-rt.com>
2012-07-12 17:46:54 +0800

    func make_treew

--- huffman.c
+++ huffman.c
@@ -30,6 +30,57 @@ static int isnew(struct node *huffman_node, int size, char new)
         return 1;
 }
 
+static int make_tree(struct node *node, int num)
+{
+        int i, j;
+
+        if (num <= 0) {
+                return -1;
+        } 
+        if (num == 1) {
+                return 0;
+        } 
+
+        // use malloc to do it 
+
+
+
+        num--;
+        return make_tree(node, num);
+/*
+        if (node == NULL || num < 0) {
+                return -1; 
+        }
+        if (num <= 1) {
+                return 0;
+        } 
+
+        for (i = 0; i < num - 1; i++) {
+                for (j = i + 1; j < num; j++) {
+                        if (node[i].priority > node[j].priority) {
+                                // exchange 
+                                node[j].priority = node[i].priority ^ node[j].priority;   
+                                node[i].priority = node[i].priority ^ node[j].priority;  
+                                node[j].priority = node[i].priority ^ node[j].priority; 
+                                
+                                node[j].data = node[i].data ^ node[j].data;
+                                node[i].data = node[i].data ^ node[j].data;
+                                node[j].data = node[i].data ^ node[j].data;
+                                
+                                node[j].rnext = node[i].rnext ^ node[j].rnext;
+                                node[i].rnext = node[i].rnext ^ node[j].rnext;
+                                node[j].rnext = node[i].rnext ^ node[j].rnext;
+                                
+                                node[j].lnext = node[i].lnext ^ node[j].lnext;
+                                node[i].lnext = node[i].lnext ^ node[j].lnext;
+                                node[j].lnext = node[i].lnext ^ node[j].lnext;
+                        } 
+                }
+        }
+        return 0;
+*/
+}
+
 
 int huffman_encode(char *encode_s)
 {
@@ -86,8 +137,12 @@ int huffman_encode(char *encode_s)
                 printf("fail to sort\n");
                 return -1;
         } 
-
-
+        
+        // make_tree
+        if (make_tree(huffman_node, huffman_size)) {
+                printf("fail to make_tree\n");
+                return -1;
+        } 
 
 
 
